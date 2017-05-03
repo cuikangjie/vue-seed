a1c322166d4f3c6a414d5ddfb45a8426567c66cc
cuikangjie <cuikangjie_90h@126.com>
2017-05-03 22:06:08 +0800

    router生命zhouqi

--- src/view/two.vue
+++ src/view/two.vue
@@ -1,6 +1,10 @@
 <template>
 <div >
     {{msg}}
+    <div>
+      传入参数为：{{id}}
+
+    </div>
 
 </div>
 </template>
@@ -9,8 +13,24 @@
 export default {
     data(){
       return {
-        msg:'this is Two !'
+        msg:'this is Two !',
+        id:''
       }
+    },
+    beforeRouteEnter(to,from,next){
+      // console.log(to.params.id);
+      next((vm)=>{
+        vm.id = to.params.id;
+      })
+    },
+    beforeRouteUpdate(to,from,next){
+      // console.log(to.params.id);
+      this.id = to.params.id;
+      next();
+    },
+    beforeRouteLeave(to,from,next){
+      alert('我要离开了');
+      next();
     }
 }
 </script>
